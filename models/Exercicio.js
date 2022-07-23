37f0c31abab5d6cf58a080316f67df85a2949ccf
Alexandre Oliveira <alext.oliver24@gmail.com>
2022-07-23 08:56:27 -0300

    Validações e mensagens de erro do schema Exercício

--- models/Exercicio.js
+++ models/Exercicio.js
@@ -1,37 +1,51 @@
 const mongoose = require('mongoose');
-// var gpsMuscular = [];
-// var equips = [];
+
+var gpsMuscular = [
+  'Bíceps',
+  'Tríceps',
+  'Antebraço',
+  'Deltóides',
+  'Dorsal',
+  'Peitoral',
+  'Abdômen',
+  'Glúteos',
+  'Quadríceps',
+  'Posterior da coxa',
+  'Panturrilha'
+];
 
 const ExercicioScheme = new mongoose.Schema(
   {
     //id: Number, // Desnecessário, pois o Mongoose gera id automaticamente
     nome: {
       type: String,
-      required: [true, 'Nome do exercício não especificado'],
-      maxLength: 20
+      required: [true, 'Nome do exercício não especificado.'],
+      maxLength: [30, 'Nome "{VALUE}" excedeu o limite de 30 caracteres.']
     },
     grupoMuscular: {
       type: [{
         type: String,
         lowercase: true,
-        // enum: gpsMuscular
-        // TODO: pesquisar e definir enum
+        enum: [
+          gpsMuscular.forEach(val => val.toLowerCase),
+          'Grupo muscular especificado não consta na lista de grupos musculares'
+        ]
       }],
       default: ["não definido"]
     },
     equipamento: {
-      type: [{
-        type: String,
-        lowercase: true
-        // TODO: definir schema para equipamentos
-      }],
-      default: ["sem equipamento"]
+      type: [String],
+      default: ["Sem equipamento"]
     },
     execucao: {
       type: String,
-      maxLength: 150
+      maxLength: [150, 'Descrição da execução excedeu o limite de 150 caracteres.'],
+      default: "Sem descrição de execução"
     },
-    descricaoVideo: String
+    descricaoVideo: {
+      type: String,
+      default: "Sem vídeo-exemplo"
+    }
   }
 );
 
