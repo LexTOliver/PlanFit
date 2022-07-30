ca9dcd079da2be18de0e0eb561b40376b1a50737
Alexandre Oliveira <alext.oliver24@gmail.com>
2022-07-30 01:29:29 -0300

    Revert "Update index.js"
    This reverts commit cf37ec1277ec1c33712d44ba56b47d790236a013.

--- index.js
+++ index.js
@@ -11,42 +11,7 @@ var alunoRoute = require('./routes/alunoRoutes.js');
 
 // Conectando ao mongoDB
 // O próprio Mongo fornece a url abaixo; TODO: configurar DB para  o projeto
-mongoose.connect(const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-// const cors = require('cors');
-
-// Rotas
-var exercicioRoute = require('./routes/exercicioRoutes.js');
-var planoRoute = require('./routes/planoRoutes.js');
-var instrutorRoute = require('./routes/instrutorRoutes.js');
-var alunoRoute = require('./routes/alunoRoutes.js');
-
-// Conectando ao mongoDB
-// O próprio Mongo fornece a url abaixo; TODO: configurar DB para  o projeto
-mongoose.connect("mongodb://username:password@host:port/database?options...", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-// Iniciando API Express e configurações
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// URL root
-app.get('/', (req, res) => {
-	res.send('PlanFit API Index! Coloque aqui sua foto do mongoose treinando!');
-});
-
-// Rotas para Exercício
-app.use('/exercicio', exercicioRoute);
-app.use('/plano', planoRoute);
-app.use('/instrutor', instrutorRoute);
-app.use('/aluno', alunoRoute);
-
-app.listen(3000, () => console.log('PlanFit API started on port 3000!'));, {
+mongoose.connect("mongodb+srv://LexOliver:cNKejpQgHrih1q1n@clustercleiton.h779g.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
