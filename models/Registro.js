5fb5fcbedfd6cbec21471eb0a3bfb82f577a0832
D-Waack <danielmwaack@gmail.com>
2022-12-20 10:20:24 -0300

    Ajuste de valores exigidos (aluno e plano)

--- models/Registro.js
+++ models/Registro.js
@@ -8,15 +8,17 @@ const RegistroSchema = new mongoose.Schema(
     },
     realizado: {
       type: Boolean,
-      default: false
+      default: false,
     },
     aluno: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Aluno',
+      required: true,
     },
     plano: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'PlanoExercicio',
+      required: true,
     }
   }
 )
